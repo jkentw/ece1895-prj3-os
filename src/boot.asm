f0d2a63d6833cac71c2a8c6074873879b8d75325
Kent <jkentw2000@gmail.com>
2022-11-30 04:01:52 -0500

    added disk read function to bootloader

--- src/boot.asm
+++ src/boot.asm
@@ -1,5 +1,5 @@
 ; J. Kent Wirant
-; 14 Nov. 2022
+; 30 Nov. 2022
 ; ECE 1895 - Project 3
 ; Bootloader
 
@@ -9,13 +9,13 @@ section .text
 bits 16
 ;org 0x7C00
 
-; ======== MASTER BOOT RECORD (MBR) ==========================================
+; ======== MASTER BOOT RECORD (MBR) ===========================================
 ; The first 512 bytes of the boot drive are loaded into memory and executed.
 ; This code/data (the MBR) must read and execute additional code from a disk 
 ; drive. This is called the "second stage" of the bootloader. The BIOS 
 ; supports basic disk I/O operations and video display functions, which this
 ; source code utilizes for loading the second stage and for status reporting.
-; ============================================================================
+; =============================================================================
 
 ; entry point of bootloader
 start_boot:
@@ -31,6 +31,17 @@ start_boot:
 	
 	; print boot message
 	mov si, str_boot
+	call print_str
+	
+	; load second stage into memory
+	mov ebx, 1 		; start from second sector (first is index 0)
+	mov cx, 15		; read 15 sectors
+	mov dl, 0x80	; hard disk drive 0 (boot drive)
+	mov si, 0x7E00	; destination address
+	call read_disk
+	
+	; print message to test success
+	mov si, str_stage2
 	call print_str
 	
 	; halt the program (best practice)
@@ -62,12 +73,65 @@ print_str:
 	popf
 	ret
 	
+; -----------------------------------------------------------------------------
+; Reads a specified number of 512-byte disk sectors into memory.
+; EBX - LBA of first sector to read
+; CX - number of sectors to read (maximum might be 0x79)
+; DL - drive number (e.g. 1st HDD is 0x80)
+; DS:SI - address to write to
+; https://en.wikipedia.org/wiki/INT_13H#INT_13h_AH=42h:_Extended_Read_Sectors_From_Drive
+read_disk:
+	pushf
+	pusha
+	
+	; populate disk address packet
+	mov [disk_address_packet_len], cx
+	lea eax, [ds:si]
+	mov [disk_address_packet_mem], eax
+	mov [disk_address_packet_lba], ebx
+	
+	; load interrupt arguments
+	xor ax, ax
+	mov ds, ax
+	mov si, disk_address_packet
+	mov ah, 0x42 	; Function number for Extended Read Sectors from Drive
+	int 0x13		; call BIOS to read disk sectors into memory
+	
+	; if no error, exit function, otherwise print error message and hang
+	jnc .success
+	mov si, str_err_read
+	call print_str
+	cli
+	hlt
+	
+.success:
+	popa
+	popf
+	ret
+	
+	
 ; DATA AND VARIABLES ----------------------------------------------------------
 
-str_boot: db 'Booting OS...', 13, 10, 0
+str_boot:					db 'Booting OS...', 13, 10, 0
+str_err_read:				db 'Disk read error!', 13, 10, 0
+
+disk_address_packet: 		db 0x10, 0		; size of DAP followed by 0
+disk_address_packet_len:	dw 0			; number of sectors to read
+disk_address_packet_mem:	dd 0			; destination address of read
+disk_address_packet_lba:	dq 0			; logical block address
 
 ; BOOT SIGNATURE --------------------------------------------------------------
 
 times (510 - $ + $$) db 00 ; padding (signature is present in last two bytes)
 dw 0xAA55 ; signature of bootable drive
-	
+
+; ======== SECOND STAGE =======================================================
+; This part of the program is present on the hard disk after the first sector.
+; The MBR calls upon the BIOS to load this part into memory. The second stage
+; of the bootloader must set up the execution environment for the operating
+; system and transition from 16-bit Real Mode into 32-bit Protected Mode.
+; =============================================================================
+
+
+; DATA AND VARIABLES ----------------------------------------------------------
+str_stage2:					db 'Second stage loaded!', 13, 10, 0
